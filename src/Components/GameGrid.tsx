608b349e9d59335899b8bde12abbca7949af1a8a
Denis <hallo-boy@mail.ru>
2023-09-28 18:58:40 +0300

    update icons. update GameGrid

---
+++ src/Components/GameGrid.tsx
@@ -0,0 +1,28 @@
+
+import { cardConvert, gameLevels } from '../mocks';
+import s from "./GameGrid.module.css"
+import React from 'react'
+
+const GameGrid = () => {
+  return (
+    <div className={s.game}>
+            {gameLevels.map((level, levelIndex) => (
+                <div key={levelIndex} className={s.level}>
+                    {level.cells.map((row, rowIndex) => (
+                        <div key={rowIndex} className={s.row}>
+                            {row.map((cell, cellIndex) => (
+                                <div key={cellIndex} className={s.cell}>
+                                    {cardConvert(cell.icon)}
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </div>
+  )
+}
+
+export default GameGrid
+
+
---
+++ src/mocks.tsx
@@ -0,0 +1,27 @@
+import { GameLevel, IconName } from "./types"
+import { FaSun, FaChrome, FaMoon, FaClock, FaCloud, FaGithub, FaOpera, FaReact } from "react-icons/fa6";
+
+
+const iconComponentMap: Record<IconName, JSX.Element> = {
+    [IconName.React]: <FaReact />,
+    [IconName.Opera]: <FaOpera />,
+    [IconName.GitHub]: <FaGithub />,
+    [IconName.Cloud]: <FaCloud />,
+    [IconName.Sun]: <FaSun />,
+    [IconName.Clock]: <FaClock />,
+    [IconName.Moon]: <FaMoon />,
+    [IconName.Chrome]: <FaChrome />,
+};
+
+
+export const cardConvert = (iconName: IconName): JSX.Element => {
+    return iconComponentMap[iconName];
+};
+
+export const gameLevels: GameLevel[] = [
+    {
+        size: 4,
+        cells: [[{ icon: IconName.React }], [{ icon: IconName.React }], [{ icon: IconName.Opera }], [{ icon: IconName.Opera }],
+        [{ icon: IconName.GitHub }], [{ icon: IconName.GitHub }], [{ icon: IconName.Cloud }], [{ icon: IconName.Cloud }], [{ icon: IconName.Sun }], [{ icon: IconName.Sun }], [{ icon: IconName.Clock }], [{ icon: IconName.Clock }], [{ icon: IconName.Moon }], [{ icon: IconName.Moon }], [{ icon: IconName.Chrome }], [{ icon: IconName.Chrome }]],
+    }
+]
